b9be4235c74a8f05663a37b34f5b370b9f241acf
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-11-17 20:33:46 +0300

    5 задание. Пояснение, переделка.

--- Isaev_Alexey_DZ4_5.py
+++ Isaev_Alexey_DZ4_5.py
@@ -2,12 +2,13 @@
 Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти четные числа
 от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать функцию reduce().
+Пояснение: элементы списка перемножить попарно.
 '''
-
 
 from functools import reduce
 
 list = [x for x in range(100, 1001) if not x % 2]
 
-prod = reduce(lambda a, b: a * b, list)
-print(prod)
+for idx, el in enumerate(list):
+    prod = reduce(lambda a, b: a * b, list[idx:idx + 2])
+    print(prod)
