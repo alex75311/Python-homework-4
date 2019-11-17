71484c0ef9abe16271ec5a35b9c4308d8d2b167a
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-11-17 15:25:40 +0300

    6 задача

---
+++ Isaev_Alexey_DZ4_6.py
@@ -0,0 +1,27 @@
+from itertools import count
+from itertools import cycle
+from time import sleep
+
+def gen_int():
+    x = int(input('Введите число с которого начать БЕСКОНЕЧНУЮ генерацию '))
+    for el in count(x):
+        print(el)
+        sleep(0.1)
+
+
+def replay_list():
+    list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x']
+
+    for el in cycle(list):
+        print(el)
+        sleep(0.1)
+try:
+    usr_ans = int(input('Введите 1 чтобы запустить БЕСКОНЕЧНЦЮ генерацию чисел или 2 чтобы запустить БЕСКОНЕЧНЫЙ повтор списка '))
+    if usr_ans == 1:
+        gen_int()
+    elif usr_ans == 2:
+        replay_list()
+    else: print('Неизвестынй ответ')
+except ValueError:
+    print('Неизвестынй ответ')
